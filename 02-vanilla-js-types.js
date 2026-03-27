a5c598cc2e060788c12d277080b764a8305e63fd
Czarina Fombuena <czarinafombuena2019@gmail.com>
2026-03-27 16:29:23 +0800

    Done playing and updating the 02-vanilla-js-types.js

--- 02-vanilla-js-types.js
+++ 02-vanilla-js-types.js
@@ -1,3 +1,9 @@
-let userName1 = "Elmer";
+let userName1 = "Czarina";
+let userAge1 = 20;
+let isEnrolled = true;
+let gpa = 1.75;
 
 console.log(typeof userName1)
+console.log(typeof userAge1)
+console.log(typeof isEnrolled)
+console.log(typeof gpa)
