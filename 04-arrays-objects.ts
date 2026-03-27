d8f2f6d3bf381b3ebc0e31746909f92001a8bc97
Czarina Fombuena <czarinafombuena2019@gmail.com>
2026-03-27 16:33:48 +0800

    Done playing and updating the 04-arrays-objects.ts file

--- 04-arrays-objects.ts
+++ 04-arrays-objects.ts
@@ -1,13 +1,8 @@
-let hobbies = ["Eating", "Watching"]
+let hobbies = ["Singing", "Gaming", "Cooking"]
 
-// hobbies.push(10)
-
-// let userList: (string | number)[];
 let userList: Array<string | number>;
 
-userList = ["Elmer", "Elmer123", 123456]
-
-// console.log(userList)
+userList = ["Czarina", "Czarina123", 202020]
 
 let user: {
     name: string;
@@ -18,23 +13,27 @@ let user: {
         id: number
     }
 } = {
-    name: "Elmer",
-    age: 31,
-    hobbies: ["Eating", "Sleeping"],
+    name: "Czarina",
+    age: 20,
+    hobbies: ["Singing", "Gaming"],
     role: {
-        description: 'admin',
-        id: 1
+        description: 'student',
+        id: 2
     }
 }
 
-// Must not null or undefined
-let val: {} = "Elmer Alvarado"
+let val: {} = "Czarina Fombuena"
 
-// Flexible Object - Record Type
 let data: Record<string, number | string>
 
 data = {
     1: 1,
-    name: 'Elmer'
-    // isPartTimeInstructor: true
+    name: 'Czarina',
+    course: 'BSIS'
 }
+
+console.log(hobbies)
+console.log(userList)
+console.log(user)
+console.log(val)
+console.log(data)
