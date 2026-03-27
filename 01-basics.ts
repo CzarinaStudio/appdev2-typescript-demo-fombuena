7b924a78901f12b9e687bc11e70e03f2adfb4241
Czarina Fombuena <czarinafombuena2019@gmail.com>
2026-03-27 16:28:21 +0800

    Done playing and updating the 01-basic.ts file

--- 01-basics.ts
+++ 01-basics.ts
@@ -1,11 +1,5 @@
-// console.log("Hello World!")
-
-let userName: string = "Elmer";
-let userAge = 31;
-
-// ...
-
-userAge = "31"
+let userName: string = "Czarina";
+let userAge = 20;
 
 console.log(`Username - ${userName}`)
 console.log(`Age - ${userAge}`)
@@ -14,7 +8,5 @@ function add(a: number, b = 5) {
     return a + b
 }
 
-add(10)
-// add('10')
-add(10, 6)
-// add(10, '6')
+console.log(add(10))
+console.log(add(10, 6))
